17f2e379958f287e78228219929547c2e99eb764
fpumir <fpumir@terredeweb.fr>
2014-01-23 12:11:22 +0100

    feature sync
    Signed-off-by: fpumir <fpumir@terredeweb.fr>

--- index.php
+++ index.php
@@ -2,34 +2,34 @@
 
 $f3=require('lib/base.php');
 $f3->set('UI','templates/');
+$f3->set('dB',new DB\SQL('mysql:host=127.0.0.1;port=3306;dbname=wtfay','root',''));
 
 $f3->route('GET /',function(){
-  //$view=new View();
-  //echo $view->render('templates/main.html');
   echo View::instance()->render('main.html');
 });
 
 $f3->route('GET /users/@alpha',function($f3){
-  
-  $datas=array(
-    'a'=>array(
-      'alain dubois',
-      'arnaud pumir',
-      'albert einstein'
-    ),
-    'b'=>array(
-      'benjamin niess',
-      'benoit seize',
-      'bibi fricotin'
-    ),
-    'c'=>array()
-  );
-  $alpha=$f3->get('PARAMS.alpha');
-  if(isset($datas[$alpha])&&is_array($datas[$alpha])){
-    $f3->set('users',$datas[$alpha]);
-  }
+  //print_r($f3->get('dB')->exec('select * from wifiloc where firstname like "'.$f3->get('PARAMS.alpha').'%"'));
+  $users=new DB\SQL\Mapper($f3->get('dB'),'wifiloc');
+  $f3->set('users',$users->find('firstname like "'.$f3->get('PARAMS.alpha').'%"'));
   echo View::instance()->render('main.html');
 });
+
+
+$f3->route('GET /user/@userId',function($f3){
+  $user=new DB\SQL\Mapper($f3->get('dB'),'wifiloc');
+  $f3->set('user',$user->load('userId="'.$f3->get('PARAMS.userId').'"'));
+  echo View::instance()->render('main.html');
+});
+
+
+
+
+
+
+
+
+
 
 
 $f3->run();
